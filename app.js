d69a6cbcbb291d022525b927efe5f37b2d9d3eb1
Miguel Quintana <miqo1385@gmail.com>
2024-01-23 19:09:13 -0800

    more files

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/user(name)?', (req, res) => {
+app.get('/user', (req, res) => {
     const username = req.params.name ? req.params.name : 'Miguel';
     res.send(`Hello, ${username}!`);
 });
