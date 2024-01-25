f66e1fe569f415876907cbfd7919b5c6269dace6
Miguel Quintana <miqo1385@gmail.com>
2024-01-24 19:48:48 -0800

    trying something new

--- express1.js
+++ express1.js
@@ -3,24 +3,14 @@ import express from "express";
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req, res) =>{
-    res.type('plain/text');
-    res.send('HELLO WORLD');
-} );
-
-app.use((req, res, next) => {
-    console.log('/foo', 'plain/text')
-    next()
+app.get('/foo', (req, res, next) => {
+    const randomResponse = Math.random() < 0.5 ? 'sometimes this' : next();
+    res.send(randomResponse);
 });
 
 app.get('/foo', (req, res) => {
-    res.send("Miguel's home page")
+    res.send('and sometimes that');
 });
-app.get('/about', (req, res) =>{
-    res.type('plain/text');
-    res.send('About Page');
-} );
-
 app.use((req, res) =>{
     res.type('plain/text');
     res.status(404);
