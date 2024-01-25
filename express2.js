36a70e5bd46c55f6d5e377dd499b4816320f7dbf
Miguel Quintana <miqo1385@gmail.com>
2024-01-24 21:36:07 -0800

    express2.js

--- express2.js
+++ express2.js
@@ -3,16 +3,13 @@ import express from "express";
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
-
 app.get('/user(name)?', (req, res) => {
-    const username = req.params.name || 'default';
-    res.send(`Hello, ${username}!`);
+    res.send('Hello, BC!');
 });
-
-
 app.use((req, res) => {
     res.status(404).send('404 - Not Found');
 });
+
 
 app.listen(app.get('port'), () =>{
     console.log('server is running');
